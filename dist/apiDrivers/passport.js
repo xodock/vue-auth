7eae9f5f82d9c019e3d58ec8edef31a06139990f
Stanislav <lawstannis@gmail.com>
2018-06-29 20:01:53 +0300

    url default fix

--- dist/apiDrivers/passport.js
+++ dist/apiDrivers/passport.js
@@ -19,7 +19,7 @@ exports.default = {
     return { access_token: access_token, refresh_token: refresh_token, expires_in: expires_in, issued_at: issued_at };
   },
   login: function login(username, password, client_id, client_secret, url, method) {
-    url = url ? url : '/oauth/token';
+    url = url ? url : 'oauth/token';
     method = method ? String(method).toLowerCase() : 'post';
 
     var body = {
@@ -35,7 +35,7 @@ exports.default = {
     return _vue2.default.login.httpDriver.methods[method](_vue2.default.login.httpInstance)(url, body);
   },
   refresh: function refresh(refresh_token, client_id, client_secret, url, method) {
-    url = url ? url : '/oauth/token';
+    url = url ? url : 'oauth/token';
     method = method ? String(method).toLowerCase() : 'post';
 
     var body = {
@@ -53,7 +53,7 @@ exports.default = {
     if (!jti) {
       return Promise.reject();
     }
-    url = url ? url : '/oauth/tokens/' + jti;
+    url = url ? url : 'oauth/tokens/' + jti;
     method = method ? String(method).toLowerCase() : 'delete';
 
     return _vue2.default.login.httpDriver.methods[method](_vue2.default.login.httpInstance)(url);
