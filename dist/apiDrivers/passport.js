4bf3813dc0ddffc69f2e11c12d2dd122d46dc2ab
Stanislav <lawstannis@gmail.com>
2018-07-04 13:42:52 +0300

    fix passport driver parse jwt issue

--- dist/apiDrivers/passport.js
+++ dist/apiDrivers/passport.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 var _vue = require('vue');
@@ -11,57 +11,63 @@ var _vue2 = _interopRequireDefault(_vue);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = {
-  parseTokenResponse: function parseTokenResponse(responseData) {
-    var access_token = responseData.access_token;
-    var refresh_token = responseData.refresh_token;
-    var expires_in = responseData.expires_in;
-    var issued_at = Math.floor(Date.now() / 1000);
-    return { access_token: access_token, refresh_token: refresh_token, expires_in: expires_in, issued_at: issued_at };
-  },
-  login: function login(username, password, client_id, client_secret, url, method) {
-    url = url ? url : 'oauth/token';
-    method = method ? String(method).toLowerCase() : 'post';
+    parseTokenResponse: function parseTokenResponse(responseData) {
+        var access_token = responseData.access_token;
+        var refresh_token = responseData.refresh_token;
+        var expires_in = responseData.expires_in;
+        var issued_at = Math.floor(Date.now() / 1000);
+        return { access_token: access_token, refresh_token: refresh_token, expires_in: expires_in, issued_at: issued_at };
+    },
+    login: function login(username, password, client_id, client_secret, url, method) {
+        url = url ? url : 'oauth/token';
+        method = method ? String(method).toLowerCase() : 'post';
 
-    var body = {
-      'grant_type': 'password',
-      'client_id': client_id,
-      'client_secret': client_secret,
-      'scope': '*'
-    };
+        var body = {
+            'grant_type': 'password',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'scope': '*'
+        };
 
-    body['username'] = username;
-    body['password'] = password;
+        body['username'] = username;
+        body['password'] = password;
 
-    return _vue2.default.login.httpDriver.getInstance()[method](url, body);
-  },
-  refresh: function refresh(refresh_token, client_id, client_secret, url, method) {
-    url = url ? url : 'oauth/token';
-    method = method ? String(method).toLowerCase() : 'post';
+        return _vue2.default.login.httpDriver.getInstance()[method](url, body);
+    },
+    refresh: function refresh(refresh_token, client_id, client_secret, url, method) {
+        url = url ? url : 'oauth/token';
+        method = method ? String(method).toLowerCase() : 'post';
 
-    var body = {
-      'grant_type': 'refresh_token',
-      'client_id': client_id,
-      'client_secret': client_secret,
-      'refresh_token': refresh_token,
-      'scope': '*'
-    };
+        var body = {
+            'grant_type': 'refresh_token',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'refresh_token': refresh_token,
+            'scope': '*'
+        };
 
-    return _vue2.default.login.httpDriver.getInstance()[method](url, body);
-  },
-  logout: function logout(access_token, url, method) {
-    var jti = parseJwt(access_token).jti;
-    if (!jti) {
-      return Promise.reject();
+        return _vue2.default.login.httpDriver.getInstance()[method](url, body);
+    },
+    logout: function logout(access_token, url, method) {
+        var jwt = parseJwt(access_token);
+        if (!jwt) {
+            return Promise.reject();
+        }
+        var jti = jwt.jti;
+        if (!jti) {
+            return Promise.reject();
+        }
+        url = url ? url : 'oauth/tokens/' + jti;
+        method = method ? String(method).toLowerCase() : 'delete';
+
+        return _vue2.default.login.httpDriver.getInstance()[method](url);
     }
-    url = url ? url : 'oauth/tokens/' + jti;
-    method = method ? String(method).toLowerCase() : 'delete';
-
-    return _vue2.default.login.httpDriver.getInstance()[method](url);
-  }
 };
 
+
 function parseJwt(token) {
-  var base64Url = token.split('.')[1];
-  var base64 = base64Url.replace('-', '+').replace('_', '/');
-  return JSON.parse(window.atob(base64));
+    if (typeof token !== 'string') return false;
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
 }
--- src/apiDrivers/passport.js
+++ src/apiDrivers/passport.js
@@ -1,57 +1,64 @@
 import Vue from 'vue';
 
 export default {
-  parseTokenResponse(responseData) {
-    let access_token = responseData.access_token;
-    let refresh_token = responseData.refresh_token;
-    let expires_in = responseData.expires_in;
-    let issued_at = Math.floor(Date.now() / 1000);
-    return {access_token, refresh_token, expires_in, issued_at}
-  },
+    parseTokenResponse(responseData) {
+        let access_token = responseData.access_token;
+        let refresh_token = responseData.refresh_token;
+        let expires_in = responseData.expires_in;
+        let issued_at = Math.floor(Date.now() / 1000);
+        return {access_token, refresh_token, expires_in, issued_at}
+    },
 
-  login(username, password, client_id, client_secret, url, method) {
-    url = url ? url : 'oauth/token';
-    method = method ? String(method).toLowerCase() : 'post';
+    login(username, password, client_id, client_secret, url, method) {
+        url = url ? url : 'oauth/token';
+        method = method ? String(method).toLowerCase() : 'post';
 
-    let body = {
-      'grant_type': 'password',
-      'client_id': client_id,
-      'client_secret': client_secret,
-      'scope': '*'
-    };
+        let body = {
+            'grant_type': 'password',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'scope': '*'
+        };
 
-    body['username'] = username;
-    body['password'] = password;
+        body['username'] = username;
+        body['password'] = password;
 
-    return (Vue.login.httpDriver.getInstance()[method])(url, body);
-  },
-  refresh(refresh_token, client_id, client_secret, url, method) {
-    url = url ? url : 'oauth/token';
-    method = method ? String(method).toLowerCase() : 'post';
+        return (Vue.login.httpDriver.getInstance()[method])(url, body);
+    },
+    refresh(refresh_token, client_id, client_secret, url, method) {
+        url = url ? url : 'oauth/token';
+        method = method ? String(method).toLowerCase() : 'post';
 
-    let body = {
-      'grant_type': 'refresh_token',
-      'client_id': client_id,
-      'client_secret': client_secret,
-      'refresh_token': refresh_token,
-      'scope': '*'
-    };
+        let body = {
+            'grant_type': 'refresh_token',
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'refresh_token': refresh_token,
+            'scope': '*'
+        };
 
-    return (Vue.login.httpDriver.getInstance()[method])(url, body);
-  },
-  logout(access_token, url, method) {
-    let jti = parseJwt(access_token).jti;
-    if (!jti) {
-      return Promise.reject()
+        return (Vue.login.httpDriver.getInstance()[method])(url, body);
+    },
+    logout(access_token, url, method) {
+        let jwt = parseJwt(access_token);
+        if (!jwt) {
+            return Promise.reject()
+        }
+        let jti = jwt.jti;
+        if (!jti) {
+            return Promise.reject()
+        }
+        url = url ? url : 'oauth/tokens/' + jti;
+        method = method ? String(method).toLowerCase() : 'delete';
+
+        return (Vue.login.httpDriver.getInstance()[method])(url);
     }
-    url = url ? url : 'oauth/tokens/' + jti;
-    method = method ? String(method).toLowerCase() : 'delete';
-
-    return (Vue.login.httpDriver.getInstance()[method])(url);
-  }
 }
-function parseJwt (token) {
-  let base64Url = token.split('.')[1];
-  let base64 = base64Url.replace('-', '+').replace('_', '/');
-  return JSON.parse(window.atob(base64));
+
+function parseJwt(token) {
+    if (typeof token !== 'string')
+        return false;
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64));
 }
